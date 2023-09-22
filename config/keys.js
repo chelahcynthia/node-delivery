a004b593e76170e396f2032633fb3fbffb44846a
chelahcynthia <cchelangat989@gmail.com>
2023-09-22 09:17:42 +0300

    Created key.js file to store the db url

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:"mongodb+srv://cchelangat989:2379@cluster0.ypi9tws.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    userRoles: ["user", "manager"]
+
+};
