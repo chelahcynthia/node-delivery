7ed3c799351b3b2c4785f8bc18185b3e73ddea6a
chelahcynthia <cchelangat989@gmail.com>
2023-09-22 09:26:55 +0300

    Successfully connected mongodb with node

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 
 
 const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true , useUnifiedTopology: true})
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
