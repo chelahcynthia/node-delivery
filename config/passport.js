3a385a4dc6e4744611d3504b802f6d494aca4ff3
chelahcynthia <cchelangat989@gmail.com>
2023-09-24 22:52:02 +0300

    Error fixes

--- config/passport.js
+++ config/passport.js
@@ -2,24 +2,23 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
-const keys = require("../config/keys");
-
+const jwtSecret = "de51f51e8c9fa9cd467d3dd7b42316f0710a0d2e8a687bde4b1dc5414bda5b25237baefe64f93d28e5c23c46cf149fff3cfce14b236b2eac79c2c5315f2421ad"; // Replace with your JWT secret key
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+opts.secretOrKey = jwtSecret;
 
-module.exports = passport => {
+module.exports = (passport) => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
-        .then(user => {
+        .then((user) => {
           if (user) {
             return done(null, user);
           }
           return done(null, false);
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     })
   );
 };
