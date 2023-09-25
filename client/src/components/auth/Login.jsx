2c0fef9e770a02e8a1177306c022f46d2a7551c0
chelahcynthia <cchelangat989@gmail.com>
2023-09-25 17:55:43 +0300

    Made necessary imports

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from classnames;
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
